4c25d46c2f8061bd0865cdac64bf4c317ca7c756
Basit <basitmanzoor438@gmail.com>
2024-10-13 15:02:13 +0530

    fixed delopment and css issues

--- src/components/ReveiwCard/ReveiwCard.jsx
+++ src/components/ReveiwCard/ReveiwCard.jsx
@@ -4,11 +4,11 @@ import '../../Screens/Home/Home.css'; // Import your custom CSS file
 
 function ReveiwCard(props) {
   return (
-    <div className="card"> {/* Use the custom class for the card */}
-      <h2>{props.name}</h2>
-      <h3>{props.position}</h3>
-      <p>{props.description}</p>
-      <p>{props.additionalInfo}</p>
+    <div>
+      <h2 className="text-lg font-bold mb-2">{props.name}</h2>
+      <h3 className="text-base text-gray-500 mb-2">{props.position}</h3>
+      <p className="mb-2 text-gray-800">{props.description}</p>
+      <p className="text-gray-600">{props.additionalInfo}</p>
     </div>
   );
 }
